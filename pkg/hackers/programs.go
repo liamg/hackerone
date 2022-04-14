c7490623b42b5a1966681bdd0beb9aec3d085731
Liam Galvin <liamgalvin@protonmail.com>
2022-04-14 11:00:15 +0100

    fix: get program (api docs are wrong)

--- pkg/hackers/programs.go
+++ pkg/hackers/programs.go
@@ -4,25 +4,25 @@ import (
 	"context"
 	"fmt"
 
-	api2 "github.com/liamg/hackerone/pkg/api"
+	"github.com/liamg/hackerone/pkg/api"
 )
 
 type getProgramsResponse struct {
-	Data  []api2.Program `json:"data"`
-	Links api2.Links     `json:"links"`
+	Data  []api.Program `json:"data"`
+	Links api.Links     `json:"links"`
 }
 
 type getProgramResponse struct {
-	Data api2.Program `json:"data"`
+	Data api.Program `json:"data"`
 }
 
 type getProgramWeaknessesResponse struct {
-	Data  []api2.Weakness `json:"data"`
-	Links api2.Links      `json:"links"`
+	Data  []api.Weakness `json:"data"`
+	Links api.Links      `json:"links"`
 }
 
 // GetPrograms returns a list of programs received by the hacker. If there are further pages, nextPage will be >0.
-func (a *API) GetPrograms(ctx context.Context, pageOptions *api2.PageOptions) (programs []api2.Program, nextPage int, err error) {
+func (a *API) GetPrograms(ctx context.Context, pageOptions *api.PageOptions) (programs []api.Program, nextPage int, err error) {
 	var response getProgramsResponse
 	path := fmt.Sprintf(
 		"/hackers/programs?page[number]=%d&page[size]=%d",
@@ -39,17 +39,17 @@ func (a *API) GetPrograms(ctx context.Context, pageOptions *api2.PageOptions) (p
 }
 
 // GetProgram returns the program for the given id.
-func (a *API) GetProgram(ctx context.Context, handle string) (*api2.Program, error) {
-	var response getProgramResponse
+func (a *API) GetProgram(ctx context.Context, handle string) (*api.Program, error) {
+	var response api.Program
 	path := fmt.Sprintf("/hackers/programs/%s", handle)
 	if err := a.client.Get(ctx, path, &response); err != nil {
 		return nil, err
 	}
-	return &response.Data, nil
+	return &response, nil
 }
 
 // GetProgramWeaknesses returns a list of weaknesses for a given program. If there are further pages, nextPage will be >0.
-func (a *API) GetProgramWeaknesses(ctx context.Context, handle string, pageOptions *api2.PageOptions) (programs []api2.Weakness, nextPage int, err error) {
+func (a *API) GetProgramWeaknesses(ctx context.Context, handle string, pageOptions *api.PageOptions) (programs []api.Weakness, nextPage int, err error) {
 	var response getProgramWeaknessesResponse
 	path := fmt.Sprintf(
 		"/hackers/programs/%s/weaknesses?page[number]=%d&page[size]=%d",
